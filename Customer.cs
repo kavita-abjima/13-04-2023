e36145f8a40d05fbf92a160bcd32faf6c131c394
kavita-abjima <128472387+kavita-abjima@users.noreply.github.com>
2023-04-13 18:31:06 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL { 
+    public class Customers
+
+    {   public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        private string _LastName;
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + _LastName;
+
+            }
+        }
+        public static int InstanceCount { get; set; }
+        public string Lastname{
+            get { return _LastName; }
+
+            set { _LastName = value; }
+        }
+    }
+    //public class CustomerTest
+    //{
+    //    public static object Assert { get; private set; }
+
+    //    public static void Main(string[] args)
+    //    {
+    //        //arrange
+    //        Customers C1 = new Customers
+    //        {
+    //            FirstName = "kavita",
+    //            Lastname = "Jangir"
+    //        };
+
+    //        Console.WriteLine(C1.FullName);
+
+    //        var c2 = new Customers();
+    //        c2.FirstName = "Kavya";
+    //        Customers.InstanceCount += 1;
+
+    //        var c3 = new Customers();
+    //        c3.FirstName = "Soniya";
+    //        Customers.InstanceCount += 1;
+
+    //        var c4 = new Customers();
+    //        c4.FirstName = "Divya";
+    //        Customers.InstanceCount += 1;
+
+    //        object value = Assert.AreEqual(3,
+    //                                       Customers.InstanceCount);
+            
+                
+
+    //    }
+    //}
+
+}
+
